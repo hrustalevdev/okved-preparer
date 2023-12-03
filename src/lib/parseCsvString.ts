c5b2707e9b2c80d4c44007e0be30495a3f755374
Timur Khrustalyov <hrustalevdev@gmail.com>
2023-12-03 11:59:09 +0300

    Add `parseCsvString`

---
+++ src/lib/parseCsvString.ts
@@ -0,0 +1,17 @@
+import { parse } from "csv-parse/sync";
+
+/**
+ * Возвращает массив массивов строк, полученных из CSV строки.
+ * @example
+ * [['section', 'code', 'name'],
+ *   ['A', '01.2', 'Выращивание многолетних культур'],
+ *   ['A', '01.21', 'Выращивание винограда']]
+ */
+export const parseCsvString = (csvString: string): string[][] => {
+  return parse(csvString, {
+    bom: true,
+    delimiter: ";",
+    trim: true,
+    skip_empty_lines: true,
+  });
+};
