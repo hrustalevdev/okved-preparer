20930cfc958f31ac31d28cb07aada6b2e6c64570
Timur Khrustalyov <hrustalevdev@gmail.com>
2023-12-03 19:52:25 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { parse } from 'csv-parse/sync';
-import iconv from 'iconv-lite';
-import util from 'util';
+import fs from "fs";
+import path from "path";
+import iconv from "iconv-lite";
 
-interface CsvRecord {
-  section: string;
-  code: string;
-  name: string;
-}
-
-const inputFile = path.resolve(__dirname, 'csv', 'okved2_1251.csv');
-const str = iconv.decode(fs.readFileSync(inputFile), 'win1251');
-// const records = parse(str, {
-//   bom: true,
-//   delimiter: ';',
-//   trim: true,
-//   skip_empty_lines: true,
-// })
-console.log('>>>RECORDS: ', str);
-
-// // Создаем объект Readable стрима для чтения файла
-// const inputStream = fs.createReadStream(inputFile);
-//
-// // Создаем объект Transform стрима для преобразования данных в UTF-8
-// const utfConverter = iconv.decodeStream('win1251');
-//
-// const parseOptions = {
-//   bom: true,
-//   delimiter: ';',
-//   trim: true,
-//   skip_empty_lines: true,
-// };
-//
-// const csvParser = parse(parseOptions);
-//
-// const data: CsvRecord[] = [];
-//
-// // Подключаем стримы: чтение из файла и преобразование в UTF-8
-// inputStream
-//   .pipe(utfConverter)
-//   .pipe(csvParser)
-//   .on('data', (record) => {
-//     const [section, code, name] = record;
-//
-//     // Добавить класс, собирающий нужный объект
-//     data.push({section, code, name});
-//   })
-//   .on('end', () => {
-//     // Сохранить результат в json файл
-//     // console.log(`Reading and decoding finished. ${util.inspect(data, {depth: null})}`);
-//   })
-//   .on('error', (error) => {
-//     console.error(`Error: ${error.message}`);
-//   });
+const inputFile = path.resolve(__dirname, "csv", "okved2_1251.csv");
+const str = iconv.decode(fs.readFileSync(inputFile), "win1251");
+console.log(">>>RECORDS: ", str);
--- src/lib/parseCsvString.ts
+++ src/lib/parseCsvString.ts
@@ -4,8 +4,8 @@ import { parse } from "csv-parse/sync";
  * Возвращает массив массивов строк, полученных из CSV строки
  * @example
  * [['section', 'code', 'name'],
- *   ['A', '01.2', 'Выращивание многолетних культур'],
- *   ['A', '01.21', 'Выращивание винограда']]
+ *  ['A', '01.2', 'Выращивание многолетних культур'],
+ *  ['A', '01.21', 'Выращивание винограда']]
  */
 export const parseCsvString = (csvString: string): string[][] => {
   return parse(csvString, {
